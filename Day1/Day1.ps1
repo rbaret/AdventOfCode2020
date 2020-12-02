435ed3d1c835cf278d56f42004d38bebfbd08f14
rbaret <richardbaret@gmail.com>
2020-12-02 22:55:19 +0100

    Initial
    Initial commit

---
+++ Day1/Day1.ps1
@@ -0,0 +1,29 @@
+﻿
+# Part 1
+Write-Host "Part 1 :"
+$values = Get-Content -LiteralPath "D:\Donnees\NextCloud\AdventOfCode\Day1.txt"
+For($i=0;$i -lt $values.Count-1;$i++){
+    For($j=$i+1;$j -lt $values.Count;$j++){
+        $sum = [int]$values[$i]+[int]$values[$j]
+        if($sum -eq 2020){ 
+            Write-Host "Values found at lines " ($i+1) " and " ($j+1) " and their product is :" ([int]$values[$i]*[int]$values[$j]).ToString();
+            break;
+        }
+    }
+}
+Write-Host "File parsed for sum of 2 values !"
+
+# Part 2
+For($i=0;$i -lt $values.Count-2;$i++){
+    For($j=$i+1;$j -lt $values.Count-1;$j++){
+        For($k=$j+1;$k -lt $values.Count;$k++)
+        {
+            $sum = [int]$values[$i]+[int]$values[$j]+[int]$values[$k]
+            if($sum -eq 2020){ 
+            Write-Host "Values found at lines " ($i+1) "," ($k+1) " and " ($j+1) " and their product is :" ([int]$values[$i]*[int]$values[$j]*[int]$values[$k]).ToString();
+            return;
+            }
+        }
+    }
+}
+Write-Host "File parsed for sum of 3 values!"
---
+++ Day2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int nbValidPwdPart1 = checkPasswordOccurences();
+            Console.WriteLine("Number of valid passwords for Part 1 : "+nbValidPwdPart1);
+            int nbValidPwdPart2 = checkPasswordPos();
+            Console.WriteLine("Number of valid passwords for Part 2 : " + nbValidPwdPart2);
+
+        }
+
+        private static int checkPasswordOccurences()
+        {
+            IEnumerable<string> lines = File.ReadAllLines(@"X:\Nextcloud (Owncloud)\Backup_fixe\Documents\Dev\AdventOfCode2020\Day2\input.txt");
+            int validPwds = 0;
+            foreach(String line in lines)
+            {
+                char[] separators = new char[]{ '-', ' ', ':' };
+                string[] fields = line.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+                int minOcc = int.Parse(fields[0]);
+                int maxOcc = int.Parse(fields[1]);
+                char character = char.Parse(fields[2]);
+                string password = fields[3];
+                int occurences =  0;
+                foreach(char letter in password)
+                {
+                    if (letter == character) occurences++;
+                }
+                if ((minOcc<=occurences) && (occurences<=maxOcc)) validPwds++;
+            }
+            return validPwds;
+        }
+        private static int checkPasswordPos()
+        {
+            IEnumerable<string> lines = File.ReadAllLines(@"X:\Nextcloud (Owncloud)\Backup_fixe\Documents\Dev\AdventOfCode2020\Day2\input.txt");
+            int validPwds = 0;
+            foreach (String line in lines)
+            {
+                char[] separators = new char[] { '-', ' ', ':' };
+                string[] fields = line.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+                int firstPos = int.Parse(fields[0]);
+                int secondPos = int.Parse(fields[1]);
+                char character = char.Parse(fields[2]);
+                string password = fields[3];
+                int position = 1;
+                int occurencesRightPos = 0;
+                foreach (char letter in password)
+                {
+                    if ((letter == character) & ((position==firstPos) | (position==secondPos))){
+                        occurencesRightPos++;
+                    }
+                    position++;
+                }
+                if (occurencesRightPos == 1) validPwds++;
+            }
+            return validPwds;
+        }
+    }
+}
