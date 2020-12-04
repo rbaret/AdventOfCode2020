c88b410ed16d605bb185cbebfdd3c5b9b896675b
rbaret <richardbaret@gmail.com>
2020-12-05 00:51:58 +0100

    Small improvement to stop

--- Day1/Day1.ps1
+++ Day1/Day1.ps1
@@ -2,26 +2,28 @@
 # Part 1
 Write-Host "Part 1 :"
 $values = Get-Content -LiteralPath "C:\Users\richard\source\repos\AdventOfCode2020\Day1\Day1.txt"
-For($i=0;$i -lt $values.Count-1;$i++){
-    For($j=$i+1;$j -lt $values.Count;$j++){
+$found = $false;
+For($i=0;($i -lt $values.Count-1) -and ($found -ne $true);$i++){
+    For($j=$i+1;($j -lt $values.Count) -and ($found -ne $true);$j++){
         $sum = [int]$values[$i]+[int]$values[$j]
         if($sum -eq 2020){ 
             Write-Host "Values found at lines " ($i+1) " and " ($j+1) " and their product is :" ([int]$values[$i]*[int]$values[$j]).ToString();
-            break;
+            $found = $true
         }
     }
 }
 Write-Host "File parsed for sum of 2 values !"
 
+$found = $false
 # Part 2
-For($i=0;$i -lt $values.Count-2;$i++){
-    For($j=$i+1;$j -lt $values.Count-1;$j++){
-        For($k=$j+1;$k -lt $values.Count;$k++)
+For($i=0;($i -lt $values.Count-2) -and ($found -ne $true);$i++){
+    For($j=$i+1;($j -lt $values.Count-1) -and ($found -ne $true);$j++){
+        For($k=$j+1;($k -lt $values.Count) -and ($found -ne $true);$k++)
         {
             $sum = [int]$values[$i]+[int]$values[$j]+[int]$values[$k]
             if($sum -eq 2020){ 
             Write-Host "Values found at lines " ($i+1) "," ($k+1) " and " ($j+1) " and their product is :" ([int]$values[$i]*[int]$values[$j]*[int]$values[$k]).ToString();
-            return;
+            $found = $true
             }
         }
     }
