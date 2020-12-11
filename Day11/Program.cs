52f7680baa9ad9f68fad6791ea99d071b82a55c5
rbaret <richardbaret@gmail.com>
2020-12-11 19:17:22 +0100

    Day 11 - Creation

---
+++ Day11/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace Day11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
