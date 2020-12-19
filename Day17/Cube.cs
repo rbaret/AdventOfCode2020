d1534034352058edd56b25a4e2afb2adb055b5e1
rbaret <richardbaret@gmail.com>
2020-12-19 02:41:29 +0100

    Day 17 \o/

---
+++ Day17/Cube.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+
+namespace Day17
+{
+    internal class Cube : IEquatable<Cube>
+    {
+        private int w;
+        private int x;
+        private int y;
+        private int z;
+        Tuple<int, int, int, int> coords;
+        private bool active;
+
+        public Cube(int w, int x, int y, int z)
+        {
+            this.w = w;
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            coords = new Tuple<int, int, int, int>(w, x, y, z);
+            active = true;
+        }
+        public Cube(int w, int x, int y, int z, bool status)
+        {
+            this.w = w;
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            coords = new Tuple<int, int, int, int>(w, x, y, z);
+            active = status;
+        }
+
+        public bool GetActiveStatus()
+        {
+            return active;
+        }
+
+        public Tuple<int, int, int, int> GetCoords()
+        {
+            return coords;
+        }
+
+        public int GetW() { return w; }
+        public int GetX() { return x; }
+        public int GetY() { return y; }
+        public int GetZ() { return z; }
+
+
+        public void SetActiveStatus(bool status)
+        {
+            active = status;
+        }
+        public override bool Equals(object obj)
+        {
+            return this.Equals(obj as Cube);
+        }
+
+        public bool Equals(Cube c)
+        {
+            if (Object.ReferenceEquals(c, null))
+            {
+                return false;
+            }
+
+            // Optimization for a common success case.
+            if (Object.ReferenceEquals(this, c))
+            {
+                return true;
+            }
+
+            // If run-time types are not exactly the same, return false.
+            if (this.GetType() != c.GetType())
+            {
+                return false;
+            }
+
+            return (w == c.w && x == c.x && y == c.y && z == c.z);
+
+        }
+
+        public bool Equals(int w, int x, int y, int z)
+        {
+            return (this.w == w && this.x == x && this.y == y && this.z == z);
+        }
+        public override int GetHashCode()
+        {
+            return w + x * 0x00010000 + y + z;
+        }
+
+        public static bool operator ==(Cube lhs, Cube rhs)
+        {
+            // Check for null on left side.
+            if (Object.ReferenceEquals(lhs, null))
+            {
+                if (Object.ReferenceEquals(rhs, null))
+                {
+                    // null == null = true.
+                    return true;
+                }
+
+                // Only the left side is null.
+                return false;
+            }
+            // Equals handles case of null on right side.
+            return lhs.Equals(rhs);
+        }
+
+        public static bool operator !=(Cube lhs, Cube rhs)
+        {
+            return !(lhs == rhs);
+        }
+        public bool IsActive()
+        {
+            return active;
+        }
+
+        public override string ToString()
+        {
+            return w.ToString()+','+x.ToString() + ',' + y.ToString() + ',' + z.ToString();
+        }
+    }
+}
---
+++ Day17/Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Day17
+{
+    static class Program
+    {
+        public static List<Cube> activeCubesList = new List<Cube>();
+        static void Main(string[] args)
+        {
+            DateTime time = DateTime.Now;
+            string path = "C:\\Users\\richard\\source\\repos\\AdventOfCode2020\\Day17\\input.txt";
+            InitializeGrid(path);
+            int activeCubes = CycleBoot3D();
+            Console.WriteLine("Cubes count after 6 3D cycles " + activeCubes);
+            InitializeGrid(path);
+            activeCubes = CycleBoot4D();
+            Console.WriteLine("Cubes count after 6 4D cycles " + activeCubes);
+
+        }
+
+        private static int CycleBoot4D()
+        {
+            List<Cube> buffer = new List<Cube>();
+            Cube currentCube;
+
+            int w, x, y, z;
+            int activeNeighbors = 0;
+            // Boot cycle
+            for (int cycle = 1; cycle <= 6; cycle++)
+            {
+                int wmin = 0 - cycle;
+                int wmax = 0 + cycle;
+                int xmin = 0 - cycle;
+                int xmax = 7 + cycle;
+                int ymin = 0 - cycle;
+                int ymax = 7 + cycle;
+                int zmin = 0 - cycle;
+                int zmax = 0 + cycle;
+                // Let's start the nested loop cycle to check surrounding cubes for each cube
+
+                for (w = wmin; w <= wmax; w++)
+                {
+                    Console.WriteLine("W dimension value : " + w);
+                    for (z = zmin; z <= zmax; z++)
+                    {
+
+                        Console.WriteLine("\tZ dimension value : " + z);
+                        for (x = xmin; x <= xmax; x++)
+                        {
+                            for (y = ymin; y <= ymax; y++)
+                            {
+                                activeNeighbors = 0;
+                                for (int wneighbor = w - 1; wneighbor <= w + 1; wneighbor++)
+                                {
+                                    for (int xneighbor = x - 1; xneighbor <= x + 1; xneighbor++)
+                                    {
+                                        for (int yneighbor = y - 1; yneighbor <= y + 1; yneighbor++)
+                                        {
+                                            for (int zneighbor = z - 1; zneighbor <= z + 1; zneighbor++)
+                                            {
+
+                                                if (activeCubesList.Contains(new Cube(wneighbor, xneighbor, yneighbor, zneighbor)) && (w != wneighbor || x != xneighbor || y != yneighbor || z != zneighbor))
+                                                {
+                                                    activeNeighbors++;
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                                currentCube = new Cube(w, x, y, z);
+                                if (activeCubesList.Contains(currentCube) && (activeNeighbors == 2 || activeNeighbors == 3))
+                                    buffer.Add(currentCube);
+                                else if (!activeCubesList.Contains(currentCube) && activeNeighbors == 3)
+                                {
+                                    buffer.Add(currentCube);
+                                }
+
+                            }
+
+                        }
+                    }
+                }
+                activeCubesList = new List<Cube>(buffer);
+                buffer.Clear();
+            }
+            return activeCubesList.Count;
+        }
+
+        private static int CycleBoot3D()
+        {
+            List<Cube> buffer = new List<Cube>();
+            Cube currentCube;
+
+            int x, y, z;
+            int activeNeighbors = 0;
+            // Boot cycle
+            for (int cycle = 1; cycle <= 6; cycle++)
+            {
+                int xmin = 0 - cycle;
+                int xmax = 7 + cycle;
+                int ymin = 0 - cycle;
+                int ymax = 7 + cycle;
+                int zmin = 0 - cycle;
+                int zmax = 0 + cycle;
+                // Let's start the nested loop cycle to check surrounding cubes for each cube
+                for (z = zmin; z <= zmax; z++)
+                {
+                    for (x = xmin; x <= xmax; x++)
+                    {
+                        for (y = ymin; y <= ymax; y++)
+                        {
+                            activeNeighbors = 0;
+                            for (int xneighbor = x - 1; xneighbor <= x + 1; xneighbor++)
+                            {
+                                for (int yneighbor = y - 1; yneighbor <= y + 1; yneighbor++)
+                                {
+                                    for (int zneighbor = z - 1; zneighbor <= z + 1; zneighbor++)
+                                    {
+                                        if (activeCubesList.Contains(new Cube(0, xneighbor, yneighbor, zneighbor)) && (x != xneighbor || y != yneighbor || z != zneighbor))
+                                        {
+                                            activeNeighbors++;
+                                        }
+                                    }
+                                }
+                            }
+                            currentCube = new Cube(0, x, y, z);
+                            if (activeCubesList.Contains(currentCube) && (activeNeighbors == 2 || activeNeighbors == 3))
+                                buffer.Add(currentCube);
+                            else if (!activeCubesList.Contains(currentCube) && activeNeighbors == 3)
+                            {
+                                buffer.Add(currentCube);
+                            }
+                        }
+                    }
+
+                }
+
+                activeCubesList = new List<Cube>(buffer);
+                buffer.Clear();
+            }
+            return activeCubesList.Count;
+        }
+
+        private static void InitializeGrid(string path)
+        {
+            activeCubesList.Clear();
+            int x = 0;
+            int y;
+            foreach (string line in File.ReadAllLines(@path))
+            {
+                y = 0;
+                char[] values = line.ToCharArray();
+                foreach (char value in values)
+                {
+                    if (value == '#')
+                        activeCubesList.Add(new Cube(0,x, y, 0, true));
+                    //else activeCubesList.Add(new Cube(x, y, 0, false));
+                    y++;
+                }
+                x++;
+            }
+
+        }
+    }
+
+
+}
